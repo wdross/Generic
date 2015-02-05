fe34e061e3c01c3e392a0cbaf48cc8e244aca8d7
Wayne Ross <mr_trekkie@yahoo.com>
2015-02-04 23:28:01 -0500

    Prevent whitespace record from being created

--- pantry/postcategory.php
+++ pantry/postcategory.php
@@ -4,25 +4,26 @@
 
 include_once 'db.php';
 
-$cat = mysql_real_escape_string($_POST["cat"]);
+$cat = trim(mysql_real_escape_string($_POST["cat"]));
 
+if (strlen($cat) > 0) {
+  //check that Category does not already exist
+  $sql_user_check = mysql_query("SELECT locate FROM locations WHERE locate='$cat'");
+  $user_check = mysql_num_rows($sql_user_check);
 
-//check that Category does not already exist
-$sql_user_check = mysql_query("SELECT locate FROM locations WHERE locate='$cat'");
-$user_check = mysql_num_rows($sql_user_check);
-
-if(($user_check == 0)){
-  $sql = mysql_query("INSERT INTO locations (locate) VALUES('$cat')")
-                      or die (mysql_error());
-  if(!$sql){
-    echo "A database error occured while adding category '.$cat.'.";
+  if ($user_check == 0) {
+    $sql = mysql_query("INSERT INTO locations (locate) VALUES('$cat')")
+                        or die (mysql_error());
+    if(!$sql){
+      echo "A database error occured while adding category '.$cat.'.";
+    }
+    else {
+      echo "<center><font face='tahoma' color='black' size='2'><b>".$cat." added to database.<br />";
+    }
+  }else{
+    echo "<center><b><font face='tahoma' color='red'>Category is already in database!</b><br><br /></center>";
   }
-  else {
-    echo "<center><font face='tahoma' color='black' size='2'><b>".$cat." added to database.<br />";
-  }
-}else{
-  echo "<center><b><font face='tahoma' color='red'>Category is already in database!</b><br><br /></center>";
-}  
-
+}
+// else quietly ignore empty/whitespace request
 include_once 'setupcategories.php';
 ?>
