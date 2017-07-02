0fa7af1ec4aa04e3e6ad860cc2fc520687528e2e
Wayne Ross <mr_trekkie@yahoo.com>
2017-07-02 02:08:42 -0400

    Allow BitObject to be a bit field too

--- Arduino/Merillat/BitObject.h
+++ Arduino/Merillat/BitObject.h
@@ -4,20 +4,29 @@
 class BitObject
 {
 public:
-  inline BitObject(INT8U *address, INT8U nbit);
-  inline bool Read();
+  // Address of byte, Position of Least Significant Bit, Field size (default 1)
+  inline BitObject(INT8U *address, INT8U lsbit, INT8U fsize=1);
+  inline INT8U Read();
+  inline void Write(INT8U value);
 
 private:
   INT8U *Address;
   INT8U Bit;
+  INT8U Mask;
 };
 
-inline BitObject::BitObject(INT8U *address, INT8U nbit) {
+inline BitObject::BitObject(INT8U *address, INT8U lsbit, INT8U fsize) {
   // save access information locally
   Address = address;
-  Bit = nbit;
+  Bit = lsbit;
+  Mask = (0x1 << fsize) - 1;
 }
-inline bool BitObject::Read() {
-  return((*Address >> Bit) & 0x1);
+inline INT8U BitObject::Read() {
+  return((*Address >> Bit) & Mask);
+}
+inline void BitObject::Write(INT8U value)
+{
+  if (Address)
+    *Address = (*Address & ~(Mask<<Bit)) | ((value&Mask) << Bit);
 }
 #endif // BITOBJECT_H
