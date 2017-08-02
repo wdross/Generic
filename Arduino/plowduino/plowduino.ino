e480fca791e4d7dd6f1e358dc2b8b401785ccd34
Wayne Ross <mr_trekkie@yahoo.com>
2017-08-02 02:09:31 -0400

    Demo program shows stable nunchuck data

---
+++ Arduino/plowduino/plowduino.ino
@@ -0,0 +1,36 @@
+#include "Wire.h"
+#include "WiiChuck.h"
+
+WiiChuck chuck = WiiChuck();
+
+void setup() {
+  Serial.begin(115200);
+  chuck.begin();
+  chuck.update();
+}
+
+void loop() {
+  delay(20);
+  chuck.update(); 
+
+  Serial.print(chuck.readJoyX());
+    Serial.print(", ");  
+  Serial.print(chuck.readJoyY());
+    Serial.print(", ");  
+
+  if (chuck.buttonZ) {
+     Serial.print("Z");
+  } else  {
+     Serial.print("-");
+  }
+
+  Serial.print(", ");  
+
+  if (chuck.buttonC) {
+     Serial.print("C");
+  } else  {
+     Serial.print("-");
+  }
+
+  Serial.println();
+}
