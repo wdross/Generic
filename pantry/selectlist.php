56bf20d2fbb572151d3a261883c04131f75d1813
Wayne Ross <mr_trekkie@yahoo.com>
2020-05-21 00:46:28 -0400

    Remove way-stale pantry: it's been in another repo for years!

--- pantry/selectlist.php
+++
@@ -1,9 +0,0 @@
-<?php
-$loclist=mysql_query("SELECT * FROM locations");
-
-while ($loc = mysql_fetch_array($loclist)) {
-  if (strlen($loc['locate']) > 0) {
-    echo '<option>'; echo $loc['locate']; echo '</option>';
-  }
-}
-?>
--- pantry/updateschema.sh
+++
@@ -1,1 +0,0 @@
-mysqldump -u root -p --no-data --databases pantrydb > schema.sql
